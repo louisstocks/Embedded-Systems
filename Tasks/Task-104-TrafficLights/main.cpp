76fd7423a2a2b8dcc6d82b523f6e8a1f48259d45
louisstocks <louis.stocks@students.plymouth.ac.uk>
2023-10-12 12:46:10 +0100

    did some work

--- Tasks/Task-104-TrafficLights/main.cpp
+++ Tasks/Task-104-TrafficLights/main.cpp
@@ -1,7 +1,7 @@
 #include "uop_msb.h"
 using namespace uop_msb;
 
-#define WAIT_TIME_MS 500 
+#define WAIT_TIME_MS 1000000 
 
 DigitalOut red(TRAF_RED1_PIN,1);         //Note the initial state
 DigitalOut amber(TRAF_YEL1_PIN,0);
@@ -16,27 +16,27 @@ int main()
     
     lcd.cls();
     lcd.puts("RED");
-    wait_us(1000000);
+    wait_us(WAIT_TIME_MS * 10);
 
     amber = 1;
     lcd.cls();
     lcd.puts("Amber");
-    wait_us(2000000);
+    wait_us(WAIT_TIME_MS * 2);
 
     red = 0;
     amber = 0;
     green = 1;
     lcd.cls();
     lcd.puts("Green");    
-    wait_us(1000000);
+    wait_us(WAIT_TIME_MS * 10);
     green = 0;
 
     for(n=0; n<=3; n = n+1){
-        wait_us(200000);
+        wait_us(WAIT_TIME_MS * 2);
         amber = 1;
         lcd.cls();
         lcd.puts("Amber");
-        wait_us(200000);
+        wait_us(WAIT_TIME_MS * 2);
         amber = 0;
     }
 
